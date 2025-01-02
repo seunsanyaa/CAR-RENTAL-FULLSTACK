b47b2a770e1080db01c4dc7f8ca9b9a49e86d9c8
ZZSavagelordZZ <khashab2004@gmail.com>
2025-01-02 06:50:23 +0200

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ClerkProvider } from '@clerk/clerk-react';
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    // <ClientClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
-    // </ClientClerkProvider>
+    </ClerkProvider>
   );
 } 
