7684fb6120990473dc8633ddad4dd7cb365d6f9b
seunsanyaa <55216847+seunsanyaa@users.noreply.github.com>
2025-01-02 06:43:04 +0200

    removed convex files

--- .idea/convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-	providers: [
-		{
-			domain: "https://divine-krill-93.clerk.accounts.dev",
-			applicationID: "convex",
-		},
-	],
-};
--- .idea/convex/util.ts
+++
@@ -1,9 +0,0 @@
-import { ActionCtx, MutationCtx, QueryCtx } from './_generated/server';
-
-export const getUserId = async (ctx: QueryCtx | MutationCtx | ActionCtx) => {
-	return (await ctx.auth.getUserIdentity())?.subject;
-};
-
-export const getUser = async (ctx: QueryCtx | MutationCtx | ActionCtx) => {
-	return await ctx.auth.getUserIdentity();
-};
